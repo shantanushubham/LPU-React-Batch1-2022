c8ebe87554a490c1457692727817a220b9e753e2
Shantanu Shubham <shantanu0707@gmail.com>
2022-01-21 08:14:55 +0530

    Initial Commit

---
+++ JavaScript/19-01.js
@@ -0,0 +1,22 @@
+// Rest and Spread Operator are applicable in Arrays and Objects in JS
+// For Object
+let object = {
+  name: "John",
+  age: 24,
+  address: {
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  },
+};
+
+let object2 = JSON.parse(JSON.stringify(object));
+object2.address.city = "Newark";
+console.log(object2);
+
+// Tomorrow we will learn
+// .map(), .forEach(), .filter(), .find(), .sort()
+// Object Destructuring in JS Objects
+// Array matching, Object matching 
+// Maps/Set  1 min
+// Classes in JS
---
+++ JavaScript/20-01.js
@@ -0,0 +1,43 @@
+// Class
+
+class Animal {
+  noOfLegs;
+  color;
+  family;
+  sound;
+
+  constructor(noOfLegs, color, family, sound) {
+    this.noOfLegs = noOfLegs;
+    this.color = color;
+    this.family = family;
+    this.sound = sound;
+  }
+
+  showAnimal() {
+    console.log(`The animal belongs to family: ${this.family}`);
+  }
+}
+
+class Dog extends Animal {
+  breed;
+  weight;
+  height;
+
+  constructor(color, breed, weight, height) {
+    super();
+    super.sound = "Woof";
+    super.noOfLegs = 4;
+    super.family = "LoyalAnimal";
+    super.color = color;
+    this.breed = breed;
+    this.weight = weight;
+    this.height = height;
+  }
+}
+
+let animal = new Animal(4, "brown", "rodent", "Phew Phew");
+animal.showAnimal();
+
+let dog = new Dog("Beige", "Corgi", "30kg", "0.5m");
+console.log(dog);
+dog.showAnimal();
